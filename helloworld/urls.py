c2d73ef4183a6db01056f6dd46901c9afc19a6f7
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2017-03-01 03:05:12 +0530

    Minor changes

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from helloworld.views import helloWorld
+from helloworld.views import helloWorld,current_datetime
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/$',helloWorld)
+    url(r'^hello/$',helloWorld),
+    url(r'^time/$',current_datetime)
 ]
+
+
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from django.http import HttpResponse
 from django.http import JsonResponse
 
@@ -6,7 +7,14 @@ json_data = open('/Users/snchit/Desktop/Sanchit/work/code/Web/helloworld/hellowo
 data = {}
 data['Name :'] = 'sanchit' 
 data['message'] = 'Hello World!!'
+data['url'] = 'http://stackoverflow.com/users/5940309/sanchit-kumar-singh'
+
 
 json_data.close()
 def helloWorld(request):
 	return JsonResponse(data)
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "ServerTime is %s." %now
+	return HttpResponse(html)
